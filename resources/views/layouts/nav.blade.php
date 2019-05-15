5ee1fc355ba71c6edc2eb465472972823193130d
tajud99n <giwa208@gmail.com>
2019-05-15 18:14:41 +0100

    profile bug fixed on nav page

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -13,9 +13,9 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Browse <span class="caret"></span> </a>
                     <ul class="dropdown-menu">
+                        @if(auth()->check())
                         <li><a href="{{ route('profile', Auth::user()) }}">Profile</a></li>
                         <li><a href="/threads">All Threads</a></li>
-                        @if(auth()->check())
                         <li><a href="/threads?by={{ auth()->user()->name }}">My Threads</a></li>
                         @endif
                         <li><a href="/threads?popular=1">Popular</a></li>
