be8f6bf603bc520c8a620fe18c77d21d0fe1862f
tajud99n <giwa208@gmail.com>
2019-05-20 14:38:02 +0100

    displaying activity feeds

---
+++ resources/views/profiles/activities/activity.blade.php
@@ -0,0 +1,13 @@
+<div class="card card-block">
+    <div class="card-heading">
+        <div class="level">
+            <span class="flex">
+                {{ $heading }}
+            </span>
+        </div>
+    </div>
+
+    <div class="card-body">
+        {{ $body }}
+    </div>
+</div>
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -10,25 +10,14 @@
                     <small>registered {{ $profileUser->created_at->diffForHumans() }}</small>
                 </h1>
             </div>
-
-            @foreach($threads as $thread)
-            <div class="card card-block">
-                <div class="card-header">
-                    <div class="level">
-                        <span class="flex">
-                            <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
-                            {{ $thread->title }}
-                        </span>
-                        <span>{{$thread->created_at->diffForHumans()}}</span>
-                    </div>
-                </div>
-                <div class="card-body">
-                    {{ $thread->body }}
-                </div>
-            </div>
+            @foreach($activities as $date => $activity)
+            <h3 class="page-header">{{ $date }}</h3>
+            @foreach($activity as $record)
+            @include("profiles.activities.{$record->type}", ['activity' => $record])
             @endforeach
-            {{ $threads->links() }}
+            @endforeach
         </div>
     </div>
 </div>
-        @endsection
+
+@endsection
